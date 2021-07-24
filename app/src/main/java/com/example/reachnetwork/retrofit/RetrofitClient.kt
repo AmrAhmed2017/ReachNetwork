97dad366d1e73a2dd9df32c886d2e9eca1f40e70
AmrAhmed2017 <aamr9465@gmail.com>
2021-07-24 17:21:05 +0200

    fixing cahe client

--- app/src/main/java/com/example/reachnetwork/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/reachnetwork/retrofit/RetrofitClient.kt
@@ -1,23 +1,32 @@
 package com.example.reachnetwork.retrofit
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import com.example.reachnetwork.App
 import okhttp3.Cache
 import okhttp3.CacheControl
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.File
-import java.util.concurrent.TimeUnit
+
 
 class RetrofitClient {
 
     fun getRetrofitClient(baseURL: String): Api{
 
-        val loggingInterceptor = HttpLoggingInterceptor()
-        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        val offlineInterceptor = Interceptor { chain ->
+            var request = chain.request()
+            if (!isInternetAvailable()) {
+                request = request.newBuilder()
+                    .cacheControl(CacheControl.FORCE_CACHE)
+                    .build()
+            }
+            chain.proceed(request)
+        }
 
         val cacheSize = 10 * 1024 * 1024
         val httpCacheDirectory = File(App.applicationContext().cacheDir, "http-cache")
@@ -25,19 +34,20 @@ class RetrofitClient {
 
         val networkCacheInterceptor = Interceptor { chain ->
             val response = chain.proceed(chain.request())
-
-            val cacheControl = CacheControl.Builder()
-                .build()
-
-            response.newBuilder()
-                .header("Cache-Control", cacheControl.toString())
-                .build()
+            val cacheControl = response.header("Cache-Control");
+            if (cacheControl == null || cacheControl.contains("no-store") || cacheControl.contains("no-cache") ||
+                cacheControl.contains("must-revalidate") || cacheControl.contains("max-age=0")) {
+                response.newBuilder()
+                    .removeHeader("Pragma")
+                    .header("Cache-Control", "public, max-age=" + 5000)
+                    .build();
+            } else response
         }
 
         val httpClient = OkHttpClient.Builder()
             .cache(cache)
             .addNetworkInterceptor(networkCacheInterceptor)
-            .addInterceptor(loggingInterceptor)
+            .addInterceptor(offlineInterceptor)
             .build()
 
         return Retrofit.Builder()
@@ -47,5 +57,14 @@ class RetrofitClient {
             .client(httpClient)
             .build()
             .create(Api::class.java)
+    }
+
+    private fun isInternetAvailable(): Boolean {
+        var isConnected = false
+        val connectivityManager = App.applicationContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+        if (activeNetwork != null && activeNetwork.isConnected)
+            isConnected = true
+        return isConnected
     }
 }
